47524f500de9104aec5e8c8965346a2322c383e5
Carlos Henrique <krlos.henrique98@gmail.com>
2026-02-05 15:36:49 -0300

    codigo alterado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Finance Manager rodando no homelab 🚀');
+  res.send('Subiu pelo github actions');
 });
 
 app.listen(3000, () => {
